18b70399eab76bd025ff98eddd92442660212f8b
Aviril <avirilaviril@protonmail.com>
2021-11-25 15:23:09 +0300

    Update instance.js

--- Server/handlers/app/instance.js
+++ Server/handlers/app/instance.js
@@ -20,18 +20,15 @@ const CInstances = {}
 ------------*/
 
 module.exports = function(socketServer, socket) {
-  console.log("LOADED MODULE..")
   socket.on("App:onClientConnect", function(userData) {
     if (!userData) return false
     if (!CInstances[(userData.uid)]) CInstances[(userData.uid)] = {}
     CInstances[(userData.uid)][this] = true
-    console.log("Client Connected..")
   })
 
   socket.on("App:onClientDisconnect", function(userData) {
     if (!userData || !CInstances[(userData.uid)]) return false
     CInstances[(userData.uid)][this] = null
-    if (Object.entries(socketDatas).length <= 0) CInstances[(userData.uid)][ = null
-    console.log("Client Disconnected..")
+    if (Object.entries(socketDatas).length <= 0) CInstances[(userData.uid)] = null
   })
 }
