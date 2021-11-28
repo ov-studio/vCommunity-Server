53aa371095493c4a1ce76c757630837cf6817cf6
Aviril <89867985+Aviril@users.noreply.github.com>
2021-11-29 01:05:51 +0300

    Update instance.js

--- Server/handlers/app/instance.js
+++ Server/handlers/app/instance.js
@@ -24,11 +24,15 @@ module.exports = function(socketServer, socket) {
     if (!userData) return false
     if (!CInstances[(userData.uid)]) CInstances[(userData.uid)] = {}
     CInstances[(userData.uid)][this] = true
+    console.log("Connected Client")
+    console.log(userData.uid)
   })
 
   socket.on("App:onClientDisconnect", function(userData) {
     if (!userData || !CInstances[(userData.uid)]) return false
     CInstances[(userData.uid)][this] = null
-    if (Object.entries(socketDatas).length <= 0) CInstances[(userData.uid)] = null
+    if (Object.entries(CInstances[(userData.uid)]).length <= 0) CInstances[(userData.uid)] = null
+    console.log("Disconnected Client")
+    console.log(userData.uid)
   })
 }
