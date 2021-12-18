b30e20f57f6591adb8f60f675dd66e0cf2796382
Aviril <89867985+Aviril@users.noreply.github.com>
2021-12-18 17:58:08 +0300

    Update database.js

--- Server/servers/database.js
+++ Server/servers/database.js
@@ -16,10 +16,15 @@ const authServer = require("firebase-admin")
 const databaseCert = (process.env["cert_database"] && JSON.parse(process.env["cert_database"])) || require("../../.cert-database.json")
 const databaseDriver = require("sequelize")
 const databaseServer = new databaseDriver(databaseCert.database)
-databaseServer.sync()
 authServer.initializeApp({
   credential: authServer.credential.cert(databaseCert.auth)
 })
+
+databaseDriver.createREF = function(defName, defData, defOptions) {
+  const createdREF = databaseServer.define(defName, defData, defOptions)
+  createdREF.sync()
+  return createdREF
+}
 databaseDriver.fetchSoloResult = function(queryResult) {
   return (queryResult && (queryResult.length > 0) && queryResult[0]) || false
 }
@@ -39,52 +44,3 @@ module.exports = {
   }
   */
 }
-
-var Posts = databaseServer.define('posSts', {
-    UID: {
-        type: databaseDriver.BIGINT,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    title: {
-      type: databaseDriver.STRING
-    },
-    content: {
-      type: databaseDriver.STRING
-    }
-  }, {});
-
-  /*
-  var Posts = databaseServer.define('posSts', {
-    UID: {
-        type: databaseDriver.BIGINT,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    DOB:{
-      type: databaseDriver.DATE,
-      defaultValue: databaseDriver.fn('NOW'),
-    },
-    title: {
-      type: databaseDriver.STRING
-    },
-    content: {
-      type: databaseDriver.STRING
-    }
-  }, {});
-  */
-
-  //moduleDependencies.server.query(`CREATE TABLE IF NOT EXISTS ${CModule.REF}("UID" TEXT PRIMARY KEY, "email" TEXT UNIQUE NOT NULL, "username" TEXT UNIQUE NOT NULL, "DOB" JSON NOT NULL, "DOC" TIMESTAMP WITH TIME ZONE DEFAULT now())`)
-  //(await CModule.REF.create(payload)).get({raw:true})
-
-  /*
-  Posts.sync({force: true}).then(async function () {
-      var lol = await Posts.create({
-        UID: 33,
-        title: 'Getting Started with PostgreSQL and Sequelize',
-        content: 'Hello there'
-      });
-      console.log(lol)
-  });
-  */
-  
