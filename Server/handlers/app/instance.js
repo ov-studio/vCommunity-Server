9ea07f5d0c898cc254134eb21e19cf78b9a681dc
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2021-12-24 01:40:37 +0300

    Finalized refactoring

--- Server/handlers/app/instance.js
+++ Server/handlers/app/instance.js
@@ -21,13 +21,13 @@ const socketInstances = {}
 -- Handlers --
 ------------*/
 
-function getInstancesByUID(UID) {
+exports.getInstancesByUID = function(UID) {
   if (!UID || !clientInstances[UID]) return false
 
   return clientInstances[UID]
 }
 
-function getInstancesBySocket(socket, fetchOnlyUID) {
+exports.getInstancesBySocket = function(socket, fetchOnlyUID) {
   const UID = socketInstances[(socket.id)]
   if (!UID) return false
 
@@ -37,30 +37,25 @@ function getInstancesBySocket(socket, fetchOnlyUID) {
   return {UID: UID, instances: cInstances}
 }
 
-module.exports = {
-  getInstancesByUID,
-  getInstancesBySocket,
+exports.injectSocket: async function(socketServer, socket) {
+  socket.on("App:onClientConnect", async function(UID) {
+    if (!UID) return false
+    if (!clientInstances[UID]) clientInstances[UID] = {}
 
-  injectSocket: async function(socketServer, socket) {
-    socket.on("App:onClientConnect", async function(UID) {
-      if (!UID) return false
-      if (!clientInstances[UID]) clientInstances[UID] = {}
+    const socketID = this.id
+    clientInstances[UID][socketID] = this
+    socketInstances[socketID] = UID
+    eventServer.emit("App:onClientConnect", this, UID)
+  })
 
-      const socketID = this.id
-      clientInstances[UID][socketID] = this
-      socketInstances[socketID] = UID
-      eventServer.emit("App:onClientConnect", this, UID)
-    })
+  socket.on("disconnect", function() {
+    const socketID = this.id
+    const UID = socketInstances[socketID]
+    if (!UID || !clientInstances[UID]) return false
 
-    socket.on("disconnect", function() {
-      const socketID = this.id
-      const UID = socketInstances[socketID]
-      if (!UID || !clientInstances[UID]) return false
-
-      delete clientInstances[UID][socketID]
-      delete socketInstances[socketID]
-      if (Object.keys(clientInstances[UID]).length <= 0) delete clientInstances[UID]
-      eventServer.emit("App:onClientDisconnect", UID)
-    })
-  }
+    delete clientInstances[UID][socketID]
+    delete socketInstances[socketID]
+    if (Object.keys(clientInstances[UID]).length <= 0) delete clientInstances[UID]
+    eventServer.emit("App:onClientDisconnect", UID)
+  })
 }
--- Server/handlers/utility.js
+++ Server/handlers/utility.js
@@ -12,32 +12,30 @@
 -- Utility Handler --
 -------------------*/
 
-module.exports = {
-  lodash: require("lodash"),
+exports.lodash = require("lodash")
 
-  sleep(milliseconds) {
-    if (!milliseconds) return false
+exports.sleep = function(milliseconds) {
+  if (!milliseconds) return false
 
-    return new Promise((resolve) => {
-      setTimeout(resolve, milliseconds)
-    })
-  },
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds)
+  })
+}
   
-  parseMS(milliseconds) {
-    if (!milliseconds) return false
+exports.parseMS = function(milliseconds) {
+  if (!milliseconds) return false
 
-    return {
-      hours: Math.floor((milliseconds/(1000*60 *60)) % 24),
-      minutes: Math.floor((milliseconds/(1000*60)) % 60),
-      seconds: Math.floor((milliseconds/1000) % 60),
-      milliseconds: parseInt((milliseconds%1000) / 100)
-    }
-  },
-
-  castDayTimeStamp(timestamp, days, isReverse) {
-    if (!timestamp || !days) return false
-
-    timestamp = new Date(timestamp)
-    return new Date(timestamp.setDate(timestamp.getDate() + (((isReverse && -1) || 1)*days)))
+  return {
+    hours: Math.floor((milliseconds/(1000*60 *60)) % 24),
+    minutes: Math.floor((milliseconds/(1000*60)) % 60),
+    seconds: Math.floor((milliseconds/1000) % 60),
+    milliseconds: parseInt((milliseconds%1000) / 100)
   }
+}
+
+exports.castDayTimeStamp = function(timestamp, days, isReverse) {
+  if (!timestamp || !days) return false
+
+  timestamp = new Date(timestamp)
+  return new Date(timestamp.setDate(timestamp.getDate() + (((isReverse && -1) || 1)*days)))
 }
